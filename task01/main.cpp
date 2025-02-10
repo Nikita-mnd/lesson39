d2379499fc6cf59f250b5ffe29c0bf1a61e49eef
Nikita-mnd <nikita.morozov.mnd@gmail.com>
2025-02-10 18:52:50 +0300

    bubble sore up

--- task01/main.cpp
+++ task01/main.cpp
@@ -1,15 +1,19 @@
 #include "util.h"
+#include "sort.h"
 #define SIZE 20
 int main() {
 
-	cout << "input size of masive: ";
-	int size;
-	cin >> size;
+	
 	int m[SIZE];
 
 	init(m, SIZE, 0, 100);
 
 	cout << "m: " << convert(m, SIZE);
+
+	 bubble_sort( m,  SIZE);
+
+	 cout << "m: " << convert(m, SIZE);
+
 
 	return 0;
 }
